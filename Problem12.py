5d20959d2a5b3a4f8eb0a552dab6ee9d08814dff
Guilherme Chimello <guilherme.chimello@gmail.com>
2016-11-18 01:26:27 -0200

    MoreTypos
    Still fixing it.

--- Problem12.py
+++ Problem12.py
@@ -1,4 +1,4 @@
-def tri2(x):	# Triangle number formula, iterative, as seen on
+def tri2(x):	# Triangle number formula, iterative,
 	x= x*(x+1)/2	# As seen on the book Things to Make and do in the fourth dimension
 	return x		# by Matt Parker
 
@@ -7,12 +7,12 @@ def primefactors(x): 	# This is a code for factor checking
 	loop=2				# Just about enough
 	while loop<=x:
 		if x%loop==0: x/=loop
-			factorlist.append(loop) 
+			factorlist.append(loop)
 		else:
-			loop+=1 
+			loop+=1
 	return factorlist
 
-def factors(s):			# The above code doesn't print the 
+def factors(s):			# The above code doesn't print the
 	xx=primefactors(s)	# non prime factors, so this one
 	facindex=[]			# adds them back in and returns the
 	for x in xx:		# number of factors the number has
@@ -20,11 +20,11 @@ def factors(s):			# The above code doesn't print the
 		if [x,z] not in facindex:
             facindex.append([x,z])
     numfac=1
-	for [p,q] in facindex: 
+	for [p,q] in facindex:
 		numfac*=(q+1)
 	return numfac
 
-for i in range(1, 15000): 				# End result is 76576500, 
+for i in range(1, 15000): 				# End result is 76576500,
 	if (factors(int(tri2(i)))>=500):	# Quite a satisfying number.
 		print(int(tri2(i)))				# as we are checking the first
 										# 15000 it also prints 103672800
