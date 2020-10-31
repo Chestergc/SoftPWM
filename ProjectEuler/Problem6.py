7a5ed90a2ee3eb2b84c30273f943770425893c24
Guilherme Chimello <guilherme.chimello@gmail.com>
2020-10-31 06:48:34 -0300

    p6

---
+++ ProjectEuler/Problem6.py
@@ -0,0 +1,7 @@
+m=100
+print( ((m*(m+1)//2)**2) - ((m*(m+1)*(2*m+1))//6) )
+##The bit before the minus is the square of the sums
+##The bit after is the sum of the squares
+##This is my first "one liner", I'm happy.
+##Yeah i know i could have hard coded the 100, but 
+##I didn't want to.
