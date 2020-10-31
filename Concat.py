d51a57082323b662e441a09c6eb93be0dd568b25
Guilherme Chimello <guilherme.chimello@gmail.com>
2020-10-31 01:44:00 -0300

    2

--- Concat.py
+++ Concat.py
@@ -3,5 +3,5 @@ import math
 def concat(x,y,b):
     return x*(b**(math.floor(math.log(y,b))+1))+y
 
-if __name__="__main__":
-    concat(20,987,10)
+if __name__=="__main__":
+    print(concat(20,987,10))
